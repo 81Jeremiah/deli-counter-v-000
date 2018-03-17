6c39add92f06a36b3c94d2690b6e55b655bf3b18
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-17 17:01:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@
 def line(katz_deli)
   if katz_deli.length > 0
     list = []
-    
+
     katz_deli.each{|name| list << katz_deli.index + name}
-  end
+  
     puts "The line is currenlty: #{list.join(" ")}"
   else
   puts "The line is currently empty."
