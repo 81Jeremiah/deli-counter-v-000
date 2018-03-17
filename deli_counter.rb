a4abab1e77f842df1d7d26f2a80d93795eace6d2
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-17 16:39:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.length > 0
+    list = []
+    number = 0
+    while number <= katz_deli.length
+    number += 1
+    katz_deli.each{|name| list << number + name}
+    end
+    puts "The line is currenlty: #{list.join}
+  else
+  puts "The line is currently empty."
+  end
+end
